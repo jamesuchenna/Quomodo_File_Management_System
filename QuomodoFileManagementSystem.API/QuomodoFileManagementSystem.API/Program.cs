645e89cb89ec4daa73468b3a491c8f2585f86fae
James Uchenna <jamesuchennachi@gmail.com>
2022-12-11 17:38:25 +0100

    Setup serilog logger

--- QuomodoFileManagementSystem.API/QuomodoFileManagementSystem.API/Program.cs
+++ QuomodoFileManagementSystem.API/QuomodoFileManagementSystem.API/Program.cs
@@ -1,25 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using Payment.API.Extensions;
+using Serilog;
 
-// Add services to the container.
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var isDevelopment = environment == Environments.Development;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+IConfiguration config = ConfigurationSetup.GetConfig(isDevelopment);
+LogSettings.SetUpSerilog(config);
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Application is starting...");
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddSingleton(Log.Logger);
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e.Message, e.StackTrace, "The application failed to start correctly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
